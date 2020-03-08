aca88847fb6f31dbcf4c10e98750ba707a33fde1
Jack Leyden <diamondsjbl@gmail.com>
2020-03-08 21:34:35 +1000

    Added search to find specific device.

--- ScannCreds - Template.py
+++ ScannCreds - Template.py
@@ -1,7 +1,14 @@
 import os
 def creds():
     scannIP = str("Your LAN IP/Subnet you wish to use '/24' is standard")
-    print(os.system('nmap -sn -v ' + scannIP))
+    print(os.system('nmap -sn -v ' + scannIP + ' > NetList.txt'))
+    
+    with open('NetList.txt') as f:#open text file
+        lines = [line.rstrip() for line in f]
+    for item in lines:#find my desired device
+        if item.find('Device to search') != -1:
+            print(item)
+            
     IP = str(input("IP :"))
     User = str("your username")
     print(os.system('ssh '+ User + '@' + IP))
--- XPServerLogin.py
+++ XPServerLogin.py
@@ -2,10 +2,18 @@ import os
 import time
 import ScannCreds
 scannIP = str(input("NetIP/Subnet :")).lower()
-if scannIP == "y":
+
+if scannIP == "y" or scannIP == "5":
     print(ScannCreds.creds())
 else:
-    print(os.system('nmap -sn -v ' + scannIP))
+    devname = str(input("DevName :"))
+    str(os.system('nmap -sn -v ' + scannIP + ' > NetList.txt'))
+    with open('NetList.txt') as f:#open text file
+        lines = [line.rstrip() for line in f]
+    for item in lines:#find my desired device
+        if item.find(devname) != -1:
+            print(item)
+    
     IP = str(input("IP :"))
     User = str(input("UserName :"))
     print(os.system('ssh '+ User + '@' + IP))
